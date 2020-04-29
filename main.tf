6581aff3c089fd0d856175f4a3fe64702b3e08ed
Nick James <njames029@gmail.com>
2020-04-28 18:17:05 -0700

    network update

--- main.tf
+++ main.tf
@@ -8,18 +8,30 @@ resource "docker_image" "nginx" {
   keep_locally = false
 }
 
+resource "docker_network" "private_network" {
+  name = "private"
+}
+
 resource "docker_container" "rust" {
   image = docker_image.rust-testfire.latest
   name  = "rust-testfire"
+  networks_advanced {
+    name = "private"
+    aliases = ["rust"]
+  }
   ports {
     internal = 4000
-    external = 4499
+    external = 4000
   }
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
   name  = "nginx"
+  networks_advanced {
+    name = "private"
+    aliases = ["nginx"]
+  }
   ports {
     internal = 80
     external = 8000
